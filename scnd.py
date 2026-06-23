4fa7ef72bfcdfb66edf056be1330c633a397a423
mhdshan-10 <muhammedshan.k123321@gmail.com>
2026-06-23 21:21:16 +0530

    Add files via upload

---
+++ scnd.py
@@ -0,0 +1,26 @@
+# variables
+a=(4)
+b=(7)
+print(a)
+print(b)
+c=(6)
+c=(10)
+c=(13)
+print(c)
+# hash means command
+# for same variable with different numbers, the number which we type as last came as result bcse python is a intrepeter lang
+# select all and ctrl+? for hash
+# data types (number data type)
+x=(11) #integer
+y=(3.7) #floating nmbr
+print(type(x))
+print(type(y))
+#STRING
+name=('mohan')
+print(name)
+g=("mohans's shirt")
+print(g)
+print(type(g))
+p=('''sanju says that " we are going "''')
+print(p)
+print(type(p))
---
+++ third.py
@@ -0,0 +1,142 @@
+#operators
+
+#addition oprts (+)
+# a=(5)
+# b=(8)
+# print (a+b)
+
+# #subtraction oprts (-)
+# a=(11)
+# b=(5)
+# print(a-b)
+
+# #division oprts (/)
+# a=(10)
+# b=(6)
+# print(a/b)
+
+#mltplctn oprts (*)
+# a=(4)
+# b=(3)
+# print(a*b)
+
+# #exponation oprts (**)
+# a=(5)
+# b=(3)
+# print(a**b)
+
+# #modulus oprts (%)
+# a=(4)
+# b=(6)
+# print(a%b)
+
+#floor oprts (//)
+# a=(10)
+# b=(4)
+# print(a//b)
+
+#boolian (true or false)
+
+#command oprts
+
+#equal to (==)
+# a=(6)
+# b=(6)
+# print(a==b)
+
+# #not equal to (!=)
+# x=(5)
+# y=(5)
+# print(x!=y)
+
+# m=(8)
+# n=(3)
+# print(m!=n)
+
+#greater than (>)
+# a=(10)
+# b=(5)
+# print(a>b)
+
+# #lessthan (<)
+# a=(5)
+# b=(10)
+# print(a<b)
+
+# #greater than or equal (>=)
+# a=(18)
+# b=(15)
+# print(a>=b)
+
+# x=(6)
+# y=(6)
+# print(x>=y)
+
+#lessthan or equal to
+# a=(5)
+# b=(10)
+# print(a<=b)
+
+# a=(10)
+# b=(5)
+# print(a<=b)
+
+# a=(8)
+# b=(8)
+# print(a<=b)
+
+
+#condition statement
+
+#if condition 
+# age=(18)
+# print(age > 18)
+# if age>17:
+#     print('eligible to vote')
+
+# age=(19)
+# print (age > 19)
+# if age > 25 :
+#     print('eligible to vote')
+# else:
+#     print('not eligible to vote')
+
+# mark = 40
+
+# if mark >30 :
+#     print('A')
+
+# if mark >20:
+#     print('B')
+
+# if mark >10:
+#     print('fail')
+
+#elif
+
+# mark = 60
+
+# if mark > 50 :
+#     print('A+')
+
+# elif mark >40 :
+#     print ('B+')
+
+# elif mark >30:
+#     print('fail')
+
+    #another example by you can change the if mark value which is larger than mark value (60)
+
+# by adding else with if and elif
+
+# mark = 90
+
+# if mark > 100:
+#     print('a plus')
+
+# elif mark > 95 :
+#     print('b plus')
+
+# else : 
+#     print('failed')
+ 
