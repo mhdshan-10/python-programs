afbfa67da5148693e907489a2cf861fed183c3b9
MUHAMMED SHAN K <muhammedshan.k123321@gmail.com>
2026-06-24 23:03:38 +0530

    Add files via upload

---
+++ four.py
@@ -0,0 +1,34 @@
+#loop (while)
+
+# a=10
+# while a>5 :
+#     print('yes')
+
+# i= 1
+# while i < 6 :
+#     print (i)
+#     i=  1 + i
+
+# sum
+
+# i= 1
+# sum = 0
+# while i < 12:
+#     sum = sum + i 
+#     i= i + 1
+#     print(sum)
+
+# FIND HOWMANY EVEN AND ODD NUMBERS ARE IN THE SUM OF 100
+# i=1
+# esum=0
+# osum=0
+# while i<=100:
+#     if i % 2 == 0:
+#         esum = esum + i
+#     else:
+#         osum = osum + i
+#     i = i+1
+# print(esum, 'evensum')
+# print(osum, 'oddsum')
+
+
