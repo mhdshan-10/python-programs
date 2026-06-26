0e25f1b92e0d60c9471ad9117e3024b4a62c5af7
MUHAMMED SHAN K <muhammedshan.k123321@gmail.com>
2026-06-26 19:53:40 +0530

    Add files via upload

---
+++ five.py
@@ -0,0 +1,44 @@
+# for loop
+
+# range (start, stop, step)
+
+# for i in range ():    ( the way to type)
+       #print
+
+# examples
+
+#for i in range (1,20,1):
+    #print(i)
+
+
+# for i in range (1,20,2):
+#     print(i)
+
+# for finding the sum of 1st 100 terms
+
+# sum=0
+# for i in range (1,101):
+#     sum = sum + i
+# print (sum)    
+    
+# for finding factorial (1x2x3x4x5x6x7x8)
+
+# fact = 1
+# for i in range (1,8+1):
+#     fact = fact * i
+# print (fact)    
+
+# count = 5
+# sum = 0   
+# for i  in range ( 1, count+1):
+#     num = int(input("enter num :-"))
+#     sum = sum + num 
+
+# print ( "sum", sum)
+# print ("average", sum/count)
+
+# pattern printing / 
+# for i in range (1,5+1):
+#     for j in range (1,i+1):
+#         print('',end='')
+#     print()
