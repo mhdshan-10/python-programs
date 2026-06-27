51f19b6dc859777925b907310d3b00e90a8461d9
MUHAMMED SHAN K <muhammedshan.k123321@gmail.com>
2026-06-27 20:07:44 +0530

    Add files via upload

---
+++ six.py
@@ -0,0 +1,79 @@
+#list
+
+# a = [1,2,3,4,5,6,7,8,9]
+
+# print(a[3])
+
+# print(a[0])
+
+# print(a[2:8])
+
+# print(a[1:7:2])
+
+# print(a[::2])
+
+# print(a[::-1])
+
+
+# b= "mohan das"
+
+# print(b[3])
+
+# print(b[5])
+
+#MUTABLE
+
+# lists are mutable  (eg)
+
+# a=[11,12,13,14,15]
+# a[3]=9
+# print(a)
+
+#string are not mutable (eg)
+
+# a="mohan"
+# a[2]=j
+# print (a)
+#(this shows error bcse string is not mutable)
+
+# append method
+
+# a=[2,4,6,8,10]
+# a.append('12')
+# print (a)
+
+# insert method
+
+# a=[1,3,5,7,11]
+# a.insert(4,'9')
+# print(a)
+
+#remove
+
+# a=[1,2,3,4,5,6,7]
+# a.remove(3)
+# print(a)
+
+# pop 
+
+# a=[21,22,23,24,25]
+# a.pop(3)
+# print(a)
+
+# a=[1,2,3,4,5]
+# a.pop() 
+# print(a)
+
+#tuple
+
+# tuples are non mutable
+
+# a=(1,2,3,4,5)
+# print(a)
+# print(type(a))
+
+# x=(1,2,3,4,5,6)
+# x[3]= 0
+# print (x)
+
+# this shows error bcse tuple does not support mutable
