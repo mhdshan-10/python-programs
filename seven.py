3a9c94e5a5f17778c66177ee323e518bd337df4e
MUHAMMED SHAN K <muhammedshan.k123321@gmail.com>
2026-07-01 20:24:23 +0530

    Add files via upload

---
+++ seven.py
@@ -0,0 +1,59 @@
+# name="mohandas"
+
+# a=[1,2,3,4,5,6]
+
+# b=("mohan", "das", "kumar" , "jyothi" , "vimal")
+
+# for i in b:
+#     print(i)
+
+# fruits= ["apple","banana","mango","watermelon"]
+# for i in range (0,len(fruits)):
+#     print(i,fruits[i])
+
+# a="fajwhjghughgzagjavghhwgsyuwsgddyrtfughghtdryfghfg"
+# for i in range (0,len(a)):
+#     print(i,a[i])
+
+# sets  
+
+# a={1,2,3,4,5,6}
+# b={4,5,6,7,8,9}
+
+# union
+
+# print(a.union(b))  #(a|b)
+
+# intersection 
+
+# print(a.intersection(b))  #(a&b)
+
+# difference
+
+# print(a.difference(b))  #(a-b) / (b-a)
+
+# dictionary
+
+# a= {"name":"mohan", "age":"20", "place":"kochi"}
+# print(a)
+# print(type(a))
+
+# mydata={}
+
+# mydata ["name"]="shan"
+# mydata ["age"]='18'
+# mydata ["place"]="kannur"
+# mydata ["number"]='987654321'
+
+# print(mydata)
+
+# an empty set also considered as a dictionary
+# me insert all this data to the empty set bcse it is also a mutable 
+
+# a= {"name":"mohan", "age":"20", "place":"kochi"}
+# a.pop("name")
+# print(a)
+
+
+
+
